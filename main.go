66567db6b700d11723a69ab82a7ffc6749c2ac04
Johannes Waigel <support@allesverhext.de>
2018-08-13 18:16:14 +0200

    Add files via upload

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"github.com/go-ini/ini"
+	"log"
+)
+
+func main()  {
+	cfg, err := ini.Load("config.ini")
+	if err != nil {
+		fmt.Printf("Fail to read file: %v", err)
+		os.Exit(1)
+	}
+
+	log.Println("App Mode: " + cfg.Section("").Key("app_mode").String())
+	StartWebserver(cfg.Section("server").Key("http_port").MustInt(9999))
+}
---
+++ webServer.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+	"strconv"
+	"fmt"
+	"github.com/holys/initials-avatar"
+)
+
+func StartWebserver(port int) {
+	log.Printf("Start webserver on port ::  %[1]d\n", port);
+	router := mux.NewRouter()
+	router.HandleFunc("/avatar", Index)
+	log.Fatal(http.ListenAndServe(":" + strconv.Itoa(port), router))
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	init :=  r.URL.Query().Get("name")
+	size, _ :=  strconv.Atoi(string( r.URL.Query().Get("size")))
+
+	if (strconv.Itoa(size) == "" ){
+		w.WriteHeader(http.StatusBadRequest)
+		return;
+	}
+	fmt.Println("error");
+
+
+
+	log.Println("Generate avatar for name :: " + init)
+	if (init ==""){
+		w.WriteHeader(http.StatusBadRequest)
+		return;
+	}
+
+	a := avatar.NewWithConfig(avatar.Config{
+		FontFile: "./ONEDAY.ttf",
+		FontSize: float64(size) * 75.0 / 128.0,
+		MaxItems: 1024,
+	})
+	b, _ := a.DrawToBytes(init, size)
+
+	w.Header().Set("Content-Type", "image/png")
+	w.Header().Set("Cache-Control", "max-age=600")
+	w.WriteHeader(http.StatusOK)
+	w.Write(b)
+}
